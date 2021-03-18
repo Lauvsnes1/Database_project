143d1dd51ce48b86f182137a226ca25867a63cd0
Even Lauvsnes <evenlau@stud.ntnu.no>
2021-03-18 14:33:36 +0100

    first

---
+++ DBConn.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+import java.util.Properties;
+
+public abstract class DBConn {
+  protected Connection conn;
+
+  public DBConn() {
+
+  }
+
+  public void connect() {
+    try {
+      // Class.forName("com.mysql.jdbc.Driver").newInstance();
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Prosjekt", "root", "fotball1");
+    } catch (Exception e) {
+      throw new RuntimeException("Unable to connect", e);
+    }
+  }
+
+}
